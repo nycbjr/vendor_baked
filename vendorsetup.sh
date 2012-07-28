fc7aea094a4f2ac6fc697354b824b4be460cd84d
travp624 <travp624@gmail.com>
2012-07-28 14:48:52 -0500

    add torospr and rearrange things a bit

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,10 @@
 add_lunch_combo baked_maguro-userdebug
 add_lunch_combo baked_toro-userdebug
-add_lunch_combo baked_tf300t-userdebug
-add_lunch_combo baked_grouper-userdebug
+add_lunch_combo baked_torospr-userdebug
 add_lunch_combo baked_crespo-userdebug
 add_lunch_combo baked_crespo4g-userdebug
+add_lunch_combo baked_grouper-userdebug
+add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_stingray-userdebug
+add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_wingray-userdebug
