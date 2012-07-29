617df7389396aeb767f821ce9bdba2258dca9e3a
travp624 <travp624@gmail.com>
2012-07-29 16:03:20 -0500

    delete duplicate entry

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,6 @@ add_lunch_combo baked_torospr-userdebug
 add_lunch_combo baked_crespo-userdebug
 add_lunch_combo baked_crespo4g-userdebug
 add_lunch_combo baked_grouper-userdebug
-add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_stingray-userdebug
 add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_wingray-userdebug
