a0a160b03a4ea2b111f34786e5c8b24f61b1ca20
travp624 <travp624@gmail.com>
2012-07-29 22:03:50 -0500

    add all things necessary for fassy

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo baked_torospr-userdebug
 add_lunch_combo baked_crespo-userdebug
 add_lunch_combo baked_crespo4g-userdebug
 add_lunch_combo baked_grouper-userdebug
+add_lunch_combo baked_fascinatemtd-userdebug
 add_lunch_combo baked_stingray-userdebug
 add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_wingray-userdebug
