73d39d39b3cc9c751562c7cdc5feaa25ae7478f2
travp624 <travp624@gmail.com>
2012-08-09 21:05:23 -0500

    alphabetically after nexus devices please for vendorsetup.sh and alph... for android.mk in products thanks

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,6 +2,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/d2tmo.mk \
+    $(LOCAL_DIR)/endeavoru.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/i9300.mk \
     $(LOCAL_DIR)/grouper.mk \
@@ -10,5 +11,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tf300t.mk \
     $(LOCAL_DIR)/toroplus.mk \
     $(LOCAL_DIR)/stingray.mk \
-    $(LOCAL_DIR)/wingray.mk \
-    $(LOCAL_DIR)/endeavoru.mk
+    $(LOCAL_DIR)/wingray.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,9 +4,9 @@ add_lunch_combo baked_toroplus-userdebug
 add_lunch_combo baked_crespo-userdebug
 add_lunch_combo baked_crespo4g-userdebug
 add_lunch_combo baked_grouper-userdebug
+add_lunch_combo baked_endeavoru-userdebug
 add_lunch_combo baked_fascinatemtd-userdebug
 add_lunch_combo baked_i9300-userdebug
 add_lunch_combo baked_stingray-userdebug
 add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_wingray-userdebug
-add_lunch_combo baked_endeavoru-userdebug
