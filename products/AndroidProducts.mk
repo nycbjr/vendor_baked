87a3e412d911ba829e22d6483c73812ec5c059ea
travp624 <travp624@gmail.com>
2012-08-16 21:32:18 -0500

    add tf201 asus transformer prime to baked

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -8,6 +8,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/toro.mk \
+    $(LOCAL_DIR)/tf201.mk \
     $(LOCAL_DIR)/tf300t.mk \
     $(LOCAL_DIR)/toroplus.mk \
     $(LOCAL_DIR)/stingray.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,6 @@ add_lunch_combo baked_endeavoru-userdebug
 add_lunch_combo baked_fascinatemtd-userdebug
 add_lunch_combo baked_i9300-userdebug
 add_lunch_combo baked_stingray-userdebug
+add_lunch_combo baked_tf201-userdebug
 add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_wingray-userdebug
