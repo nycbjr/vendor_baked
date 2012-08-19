83e94b6e3edea20b5ad06b1b9b9dff59e876b4d9
travp624 <travp624@gmail.com>
2012-08-19 15:05:38 -0500

    add otaupdater and bump to bb-3 for when its ready.

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,10 +4,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-2
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-3
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=BAKED \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=2
+    ro.goo.version=3
+
+# OTA updater
+PRODUCT_PROPERTY_OVERRIDES += \
+    otaupdater.otaid=baked0720118490 \
+    otaupdater.otatime=20120819-0127 \
+    otaupdater.otaver=blackbean-3
