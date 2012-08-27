7d6508b29932f205e123d405d6f7a053e8f69b81
travp624 <travp624@gmail.com>
2012-08-26 23:15:17 -0500

    cleanup behind brett lol. Also vendorsetup.sh alphabetically after nexus devices please.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ add_lunch_combo baked_grouper-userdebug
 add_lunch_combo baked_endeavoru-userdebug
 add_lunch_combo baked_fascinatemtd-userdebug
 add_lunch_combo baked_i9300-userdebug
+add_lunch_combo baked_p3113-userdebug
 add_lunch_combo baked_stingray-userdebug
 add_lunch_combo baked_tf201-userdebug
 add_lunch_combo baked_tf300t-userdebug
 add_lunch_combo baked_wingray-userdebug
-add_lunch_combo baked_p3113-userdebug
