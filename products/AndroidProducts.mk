521843d3e6729992ab2344bb6efe9417c4ab28ea
travp624 <travp624@gmail.com>
2012-09-04 22:10:34 -0500

    add the entire galaxy line showcase mez vib etc.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,9 +1,12 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/d2tmo.mk \
+    $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/endeavoru.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
+    $(LOCAL_DIR)/galaxymtd.mk \
     $(LOCAL_DIR)/i9300.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/maguro.mk \
@@ -13,4 +16,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tf300t.mk \
     $(LOCAL_DIR)/toroplus.mk \
     $(LOCAL_DIR)/stingray.mk \
+    $(LOCAL_DIR)/vibrantmtd.mk \
     $(LOCAL_DIR)/wingray.mk
