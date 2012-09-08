036cd83c0c98f2e059d5a7a3bd0e47a9b2f40b23
travp624 <travp624@gmail.com>
2012-09-07 20:49:25 -0500

    add sprints s3 to the mix

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,6 +2,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
+    $(LOCAL_DIR)/d2spr.mk \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/endeavoru.mk \
---
+++ products/d2spr.mk
@@ -0,0 +1,22 @@
+$(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/baked/config/cdma.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/baked/config/common_phone.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/baked/overlay/d2vzw
+
+PRODUCT_NAME := baked_d2spr
+PRODUCT_DEVICE := d2spr
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := d2spr
+PRODUCT_MODEL := SPH-L710
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2spr TARGET_DEVICE=d2spr BUILD_FINGERPRINT="d2spr-user 4.0.4 IMM76D L710VPALG2 release-keys" PRIVATE_BUILD_DESC="samsung/d2spr/d2spr:4.0.4/IMM76D/L710VPALG2:user/release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+	vendor/baked/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
