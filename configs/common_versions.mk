4ec0c2a20bd0417faf2bf6d27ccd094623f43a23
travp624 <travp624@gmail.com>
2012-09-08 15:25:26 -0500

    yes i'm updating to bb4 already. well for whenever its ready

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,17 +4,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-3
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-4
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=BAKED \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=3
+    ro.goo.version=4
 
 # OTA updater
 DATE = $(shell date +"%Y%m%d-%H%M")
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(TARGET_PRODUCT) \
     otaupdater.otatime=$(DATE) \
-    otaupdater.otaver=blackbean-3
+    otaupdater.otaver=blackbean-4
