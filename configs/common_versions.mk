87624b7496f82168efa540e7a568630c5285e0de
travp624 <travp624@gmail.com>
2012-09-28 21:12:25 -0500

    bb5 for when its ready

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,17 +4,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-4
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-5
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=BAKED \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=4
+    ro.goo.version=5
 
 # OTA updater
 DATE = $(shell date +"%Y%m%d-%H%M")
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(TARGET_PRODUCT) \
     otaupdater.otatime=$(DATE) \
-    otaupdater.otaver=blackbean-4
+    otaupdater.otaver=blackbean-5
