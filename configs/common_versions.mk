29684ec75008824e7ccfd8e606a38a94f9af1927
travp624 <travp624@gmail.com>
2012-09-30 13:41:54 -0500

    changes to the versioning number making it easier to update

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,20 +1,22 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+# build version number
+BUILD VERSION = 5
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-5
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-$(BUILD VERSION)
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=BAKED \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=5
+    ro.goo.version=$(BUILD VERSION)
 
 # OTA updater
 DATE = $(shell date +"%Y%m%d-%H%M")
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(TARGET_PRODUCT) \
     otaupdater.otatime=$(DATE) \
-    otaupdater.otaver=blackbean-5
+    otaupdater.otaver=blackbean-$(BUILD VERSION)
