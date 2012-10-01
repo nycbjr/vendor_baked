a2ef04ae8244a38315d50445d6a4ae8006ff414c
travp624 <travp624@gmail.com>
2012-09-30 21:04:51 -0500

    fix for buildbot uploads for now.

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,21 +2,21 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # build version number
-BUILD VERSION = 5
+BUILD_VERSION = 5
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-$(BUILD VERSION)
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-5
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=BAKED \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=$(BUILD VERSION)
+    ro.goo.version=$(BUILD_VERSION)
 
 # OTA updater
 DATE = $(shell date +"%Y%m%d-%H%M")
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(TARGET_PRODUCT) \
     otaupdater.otatime=$(DATE) \
-    otaupdater.otaver=blackbean-$(BUILD VERSION)
+    otaupdater.otaver=blackbean-$(BUILD_VERSION)
