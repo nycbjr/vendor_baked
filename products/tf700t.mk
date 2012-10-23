58811fa015bbb256e9c4b10db2277fd61d3d7cd4
nycbjr <nybjr@gmail.com>
2012-10-22 22:24:17 -0400

    updating tf700 build dedescription

--- products/tf700t.mk
+++ products/tf700t.mk
@@ -12,13 +12,13 @@ PRODUCT_BRAND := asus
 PRODUCT_DEVICE := tf700t
 PRODUCT_MODEL := Transformer Pad TF700T
 PRODUCT_MANUFACTURER := asus
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/WW_epad/EeePad:4.0.3/IML74K/WW_epad-9.4.3.29-20120511:user/release-keys PRIVATE_BUILD_DESC="WW_epad-user 4.0.3 IML74K WW_epad-9.4.3.29-20120511 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.1.1/JRO03C/US_epad-10.4.4.18-20121012:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.1.1 JRO03C US_epad-10.4.4.18-20121012 release-keys"
 
 # Release name and versioning
 # PRODUCT_RELEASE_NAME := tf700t
 
 # bootanimation
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
 	vendor/baked/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation.zip
 
 # Additional APK to install
