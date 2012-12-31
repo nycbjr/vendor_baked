e5855da0f26efe1be4c2a9a17b419110c42c1e2c
Chuck Haines <chaines@codemonkeyjava.com>
2012-12-31 14:35:22 -0600

    Added LockClock into build.
    Signed-off-by: Chuck Haines <chaines@codemonkeyjava.com>

--- products/grouper.mk
+++ products/grouper.mk
@@ -18,4 +18,5 @@ PRODUCT_COPY_FILES += \
     vendor/baked/prebuilt/bootanimation/bootanimation_800_1280.zip:system/media/bootanimation.zip
 
 PRODUCT_PACKAGES += \
-    Supernote
+    Supernote \
+    CMFileManagerThemes
