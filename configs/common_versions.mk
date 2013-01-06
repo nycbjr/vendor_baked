8d0561a6e9ef9cde09b442207b33b987a4d88b7d
travp624 <travp624@gmail.com>
2013-01-06 02:28:12 -0600

    rc time oh boy

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,11 +2,11 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # build version number
-BUILD_VERSION = 7-test
+BUILD_VERSION = 7-rc
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-7-test
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-7-rc
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
