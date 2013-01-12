a4e94430b2de5bdec3d94ec0be09d17b15907300
travp624 <travp624@gmail.com>
2013-01-12 13:52:50 -0600

    Bump sorta version and cleanup common_version makefile
    Signed-off-by: travp624 <travp624@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,22 +1,22 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # build version number
-BUILD_VERSION = 7-rc
+BAKED_VERSION = 7
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_blackbean-7-rc
+    ro.baked.version=$(TARGET_PRODUCT)_blackbean-7
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=BAKED \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=$(BUILD_VERSION)
+    ro.goo.version=$(BAKED_VERSION)
 
 # OTA updater
 DATE = $(shell date +"%Y%m%d-%H%M")
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(TARGET_PRODUCT) \
     otaupdater.otatime=$(DATE) \
-    otaupdater.otaver=blackbean-$(BUILD_VERSION)
+    otaupdater.otaver=blackbean-$(BAKED_VERSION)
