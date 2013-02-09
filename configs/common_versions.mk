850781d70a48d99e2d1e8f1bbbf5d603139a1eaa
travp624 <travp624@gmail.com>
2013-02-09 12:56:10 -0600

    bb 8 time
    Signed-off-by: travp624 <travp624@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,11 +2,11 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # build version number
-BAKED_VERSION = 7.1
+BAKED_VERSION = 8
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.baked.version=$(TARGET_PRODUCT)_bb-$(BAKED_VERSION)
+    ro.baked.version=$(TARGET_PRODUCT)_bb-8
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -15,7 +15,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.version=$(BAKED_VERSION)
 
 # OTA updater
-DATE = 20130123-1200
+DATE = 20130209-1300
+
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(TARGET_PRODUCT) \
     otaupdater.otatime=$(DATE) \
